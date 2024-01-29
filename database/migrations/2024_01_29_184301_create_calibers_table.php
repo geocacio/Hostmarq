484db3c20e47e1b5f7319f494f5b9884396282b2
Cácio Dias <cacio@gginformaticaweb.com>
2024-01-29 15:55:37 -0300

    adiciona as colunas no calibre

--- database/migrations/2024_01_29_184301_create_calibers_table.php
+++ database/migrations/2024_01_29_184301_create_calibers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('calibers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->enum('type', ['permitted', 'restricted']);
+            $table->string('slug');
             $table->timestamps();
         });
     }
