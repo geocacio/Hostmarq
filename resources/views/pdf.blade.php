1dcd372642c2333b5434a7ce2914f008560e9b5c
Jair Barreto <jairbarreto549@gmail.com>
2024-03-15 11:42:20 -0300

    Cria tamplate para pdf

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF</title>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet"/>
+</head>
+<body>
+    <div>
+        <div class="header-pdf">
+            <img src="https://sistema.hostmarq.com.br/img/logo_site.png"/>
+            <span><b>HOSTMARQ - SOLUÇÕES WEB</b></span>
+            <span>CNPJ: 47.559.397/0001-10</span>
+            <span>RUA FIRMINO FERREIRA GOMES 737 - AEROPORTO - MOSSORÓ/RN</span>
+            <span>N° CR: 0 - Telefone: 84-9-8121-2123</span>
+            <span>Email: contatos@hostmarq.com.br</span>
+        </div>
+        <h1>RELAÇÃO DE MEMBROS POR DATA DE FILIAÇÃO</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Atirador</th>
+                    <th>Endereço</th>
+                    <th>Documentos</th>
+                    <th>D.Afiliação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <span><b>0001</b></span>
+                        <span>Magno Carlos Gomes Mendonça</span>
+                    </td>
+                    <td>
+                        <span>Rua Firmino Ferreira Gomes 33</span>
+                        <span>Aeroporto - CEP: 59607-812</span>
+                        <span>Mossoró/RN</span>
+                    </td>
+                    <td>
+                        <span>CPF: 87714132434</span>
+                        <span><a href="mailto: wpmarques@outlook.com">wpmarques@outlook.com</a></span>
+                        <span>84-9-8121-2122</span>
+                    </td>
+                    <td>
+                        <span>01/02/2022</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <p>
+            O HOSTMARQ - DESENVOLVIMENTO DE SISTEMAS, inscrito no CPF: 877.141.324-34 e
+            Certificado de Registro n? CR 199275, com sede na rua Firmino Ferreira Gomes 737, bairro
+            Aeroporto - Cep 59.607-812 - Mossor?/RN, DECLARA, que Magno Carlos Gomes Mendonça,
+            CPF:87714132434, RG: 889455 , SSP/RN, CR: 774890, está regularmente FILIADO nesta
+            Entidade sob o Nº 0001, datado de 01/02/2022, e que o mesmo está iniciando a pratica de tiro na
+            atividade Atirador Desportivo, portanto, ainda não possui as participações mínimas.
+        </p>
+        <p>Esta declaração tem validade de 90 dias.</p>
+    </div>
+    <div class="footer-pdf">
+        <p class="address-footer">Mossoró (RN), 15 de Março de 2024.</p>
+
+        <span>______________________________________________</span>
+        <p class="signature">REQUERENT</p>
+        <span><b>Magno Carlos Gomes Mendonça</b></span>
+        <p class="signature">CPF: 877.141.324-34</p>
+    </div>
+</body>
+</html>
